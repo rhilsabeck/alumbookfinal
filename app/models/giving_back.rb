e03c1c41ea4d5dd4c6ede141aa74abf55f332da2
Ryan Hilsabeck <ryan.hilsabeck@gmail.com>
2015-11-23 18:22:26 -0600

    Final Demo Fixes
    Added program concentrations to all program drop downs. Back to
    unpublished surveys button(when you are in the edit view of an
    unpublished survey) now works. Removed Additional Personal Information
    section from the user profile. Corrected all Date_select year fields to
    start scrolling from current time period. Waiting for mentoring staying
    connected blurb and more degree types.

--- app/models/giving_back.rb
+++ app/models/giving_back.rb
@@ -2,7 +2,7 @@
 class GivingBack < ActiveRecord::Base
   acts_as_paranoid
   self.inheritance_column = nil # Allows the type column to be used without interfering with Rails' conventions
-  default_scope { order(created_at: :desc, type: :asc) } # Sort by date created in descending order by default
+  default_scope { order(type: :asc, created_at: :desc) } # Sort by date created in descending order by default
   scope :pending, -> { where(hidden: false, contacted: false, approved: false) }
   scope :contacted, -> { where(contacted: true, hidden: false) }
   scope :approved, -> {where(approved: true, hidden: false)}
